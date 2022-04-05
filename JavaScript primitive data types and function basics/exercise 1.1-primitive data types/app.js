895c09ebec24f5596535f9d4e905adc06bee7bdf
shirtol <shirzahavi7@gmail.com>
2022-04-05 22:17:28 +0300

    Added exercise 1.1 JS

---
+++ JavaScript primitive data types and function basics/exercise 1.1-primitive data types/app.js
@@ -0,0 +1,35 @@
+let myName = "Shir";
+
+let age = 27;
+
+const myObject = {
+    firstName: "Shir",
+    lastName: "Toledano",
+};
+
+let myObjectTwo = Object(8);
+
+let isHere = true;
+
+let holdsNull = null;
+
+let undefinedVar;
+
+let symbolVar = Symbol("hi");
+
+console.log(myName);
+console.log(typeof myName);
+console.log(age);
+console.log(typeof age);
+console.log(myObject);
+console.log(typeof myObject);
+console.log(myObjectTwo);
+console.log(typeof myObjectTwo);
+console.log(isHere);
+console.log(typeof isHere);
+console.log(holdsNull);
+console.log(typeof holdsNull);
+console.log(undefinedVar);
+console.log(typeof undefinedVar);
+console.log(symbolVar);
+console.log(typeof symbolVar);
