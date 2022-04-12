cca4cc9a0bba5244d98daeb6bcf591bbbf603a07
shirtol <shirzahavi7@gmail.com>
2022-04-12 23:59:01 +0300

    Added to exercise 9.2

--- JavaScript- loops and callback functions/exercise 9.2-while_loop/app.js
+++ JavaScript- loops and callback functions/exercise 9.2-while_loop/app.js
@@ -1,5 +1,6 @@
 // 9.2.1
 
+// If we ment to recreate the challenge from exercise 6.4 (looping with arrays):
 const populations = [
     125.8, 9.217, 59.55, 9.75,
 ]; /*Japan, Israel, Italy, hungary */
@@ -18,6 +19,22 @@ const populationPercentages = (populationArr) => {
 };
 
 console.log(populationPercentages(populations));
+
+// If we ment to recreate the challenge from exercise 6.3 (length of words):
+const exampleArr = ["boo", "doooo", "hoo", "ro"];
+
+const getArrOfLengths = (arrOfStrings) => {
+    const arrOfLengths = [];
+    let i = 0;
+    while (i < arrOfStrings.length) {
+        arrOfLengths[i] = arrOfStrings[i].length;
+        i++;
+    }
+
+    return arrOfLengths;
+};
+
+console.log(getArrOfLengths(exampleArr));
 
 // 9.2.2
 /* The solution I liked better is the for loop, it's more intuitive for me and I think creates less bugs in this task. */
