6cacbe6285bfe84a797d977d19a27d69c8f6febd
shirtol <shirzahavi7@gmail.com>
2022-04-17 21:39:05 +0300

    Changed slice to spread operator

--- Javascript-sort and reduce/exercise 13.1-sort_numbers/app.js
+++ Javascript-sort and reduce/exercise 13.1-sort_numbers/app.js
@@ -1,10 +1,10 @@
 const numbers = [1, -5, 666, 2, 400, 11];
 
 // 13.1.1: ascending order
-const ascendingSort = (arr) => arr.slice().sort((num1, num2) => num1 - num2);
+const ascendingSort = (arr) => [...arr].sort((num1, num2) => num1 - num2);
 
 // 13.1.2: descending order
-const descendingSort = (arr) => arr.slice().sort((num1, num2) => num2 - num1);
+const descendingSort = (arr) => [...arr].sort((num1, num2) => num2 - num1);
 
 console.log(ascendingSort(numbers));
 console.log(descendingSort(numbers));
