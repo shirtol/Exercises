a6e277e7404744d4bdada1dec650290be88276fb
shirtol <shirzahavi7@gmail.com>
2022-04-17 10:30:01 +0300

    Added exercise 13.1

---
+++ Javascript-sort and reduce/exercise 13.1-sort_numbers/app.js
@@ -0,0 +1,11 @@
+const numbers = [1, -5, 666, 2, 400, 11];
+
+// 13.1.1: ascending order
+const ascendingSort = (arr) => arr.slice().sort((num1, num2) => num1 - num2);
+
+// 13.1.2: descending order
+const descendingSort = (arr) => arr.slice().sort((num1, num2) => num2 - num1);
+
+console.log(ascendingSort(numbers));
+console.log(descendingSort(numbers));
+console.log(numbers);
