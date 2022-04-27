bedb34f3c3cfd874d9bb3687180c54c88f080a05
shirtol <shirzahavi7@gmail.com>
2022-04-27 18:31:22 +0300

    Refactoring Ex24.1

--- JavaScript-This-keyword-and-prototypes/exercise24.1-is_square_aTJjklj/app.js
+++ JavaScript-This-keyword-and-prototypes/exercise24.1-is_square_aTJjklj/app.js
@@ -6,13 +6,11 @@ function Square(a, b, c, d) {
 }
 
 Square.prototype.isSquare = function () {
-    if (this.a === this.b && this.d === this.c && this.a === this.c)
-        return true;
-    return false;
+    return this.a === this.b && this.d === this.c && this.a === this.c;
 };
 
 const mySquare = new Square(4, 4, 4, 4);
-const mySquare2 = new Square(4, 4, 3, 4);
+const mySquare2 = new Square(4, 3, 3, 4);
 
 console.log(mySquare.isSquare());
 console.log(mySquare2.isSquare());
