df14f4adb82948d2bc6bfd56b43e333575cafde5
shirtol <shirzahavi7@gmail.com>
2022-04-27 18:31:04 +0300

    Refactoring Ex23.1

--- JavaScript-This-keyword-and-prototypes/exercise23.1-fundamentals of this/app.js
+++ JavaScript-This-keyword-and-prototypes/exercise23.1-fundamentals of this/app.js
@@ -1,9 +1,9 @@
 //! Q1:
-/*console.log(this); will point to the window because it is in the global scope.*/
+/*"this" will point to the window because it is in the global scope.*/
 
 //! Q2
 /*
- *a. this will points to the window because the method is an arrow function.
+ *a. "this" will point to the window because the method is an arrow function.
  *b.We can fix it by using function declaration:
  */
 
@@ -18,21 +18,27 @@ myObj.greet();
 
 // !Q3
 /*
-It is function expression, hence this will refer to the window object 
+It is a function in the global scope, hence "this" will refer to the window object 
 */
 
 // !Q4
 /*
-It is an arrow function so this will refer to the window object.
+It is an arrow function so "this" will refer to the window object.
 */
 
 // !Q5
 /*
- *a. This will refer to the window because of the arrow function. (It throw an error in the browser because we 
-    didn't pass the event to addEventListener function)
- *b. We can fix it by adding the event and using function declaration. //TODO: Try to understand what's going on here..
+ *a. "this" will refer to the window because of the arrow function. (It throws an error in the browser because we
+ *didn't pass the event to addEventListener function)
+ *b. We can fix it by adding the event and using function declaration (and then "this" will point to the element.).
  */
 
-document.querySelector(".element").addEventListener("click", () => {
+//  Points to the window
+// document.querySelector(".element").addEventListener("click", () => {
+//     console.log(this);
+// });
+
+// Points to the element.
+document.querySelector(".element").addEventListener("click", function () {
     console.log(this);
 });
